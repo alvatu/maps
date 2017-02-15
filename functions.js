ae4ab480971e24d922ac3594b59354bf26b9e119
alvatu <abarrancoturienzo@gmail.com>
2017-02-15 23:56:15 +0100

    Add files via upload

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-var map, lat, lng, posicioninicial,geocoder,contentString,datosactuales,datosfuturos,marker;
+var map, posicioninicial;
 var apikey = "d890118bdd72ea2c15a196a419e3f82b"; //clave de acceso para la api openweathermap
  
 function comenzar(){
@@ -13,12 +13,12 @@ function comenzar(){
   // guarda coords en lat y lng
   GMaps.geolocate({
     success: function(position){
-       lat = position.coords.latitude;  
-       lng = position.coords.longitude;
+       var lat = position.coords.latitude;  
+       var lng = position.coords.longitude;
        posicioninicial = [lat,lng];    //guardo en posicioninicial las coordenadas iniciales
        crearmapa(lat,lng);      // muestra mapa centrado en coords [lat, lng]
        map.addMarker({ lat: lat, lng: lng});   // marcador en [lat, lng]
-       geocoder = new google.maps.Geocoder();
+       
     },
     error: function(error) { alert('Geolocalización falla: ' + error.message); },
     not_supported: function(){ alert("Su navegador no soporta geolocalización"); },
@@ -40,13 +40,13 @@ function seleccionarpunto(e){
   map.removeMarkers();    // borramos el marcador anterior
   lat = e.latLng.lat();   // guardamos los valores de la latitud y la longitud para dibujarlos en el mapa
   lng = e.latLng.lng();
-  marker = map.addMarker({ lat: lat, lng: lng});   // ponemos el nuevo marcador con las coordenadas actuales
+  
 
-  $.ajax({
+  $.ajax({   // se realiza la llamada a la api de metereologia y se guarda la informacion relativa al tiempo actual
     url: "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lng + "&appid=" + apikey + "&units=metric&lang=es",
     success: function( data ) {
-      datosactuales = data;
-          procesaDatos();
+       var datosactuales = data;
+          procesaDatos(datosactuales);
     },
     error: function(){
       alert("¡Ups! No puedo obtener información de la API");
@@ -54,13 +54,13 @@ function seleccionarpunto(e){
   });
 };
 
-function procesaDatos(){
+function procesaDatos(datosactuales){
 
- $.ajax({
+ $.ajax({   // se realiza la llamada a la api de metereologia y se guarda la informacion relativa al pronostico del tiempo
     url: "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lng + "&appid=" + apikey + "&units=metric&lang=es",
     success: function( data ) {
-      datosfuturos = data;
-          muestraDatos();
+     var datosfuturos = data;
+          muestraDatos(datosactuales,datosfuturos);
     },
     error: function(){
       alert("¡Ups! No puedo obtener información de la API");
@@ -68,9 +68,10 @@ function procesaDatos(){
   });
 }
 
-function muestraDatos(){    // mostramos los datos en el pop up
-  
-  contentString = "<div>Temperatura actual: " + datosactuales.main.temp +  " ºC </div> <div> Tempetatura mañana: " + datosfuturos.list[1].main.temp_max + " ºC </div>";
+function muestraDatos(datosactuales,datosfuturos){    // mostramos los datos en el pop up
+
+  var marker = map.addMarker({ lat: lat, lng: lng});   // ponemos el nuevo marcador con las coordenadas actuales
+  var contentString = "<div>Temperatura actual: " + datosactuales.main.temp + " ºC - Nubosidad: " + datosactuales.clouds.all + " % </div> <br> <div>Tª media mañana: " + datosfuturos.list[0].main.temp + " ºC - Nubosidad mañana: " + datosfuturos.list[0].clouds.all + " % </div>  <br> <div>Tª media pasado mañana: " + datosfuturos.list[1].main.temp + " ºC - Nubosidad pasado mañana: " + datosfuturos.list[1].clouds.all + " % </div>";
   var infowindow = new google.maps.InfoWindow({content: contentString});
   infowindow.open(map, marker);
 }
@@ -84,16 +85,16 @@ function inicializar(){     // mostramos el mapa en la posicion inicial
 
 function buscar() {   // buscamos las coordenadas de una direccion y la buscamos en el mapa
 
+  var geocoder = new google.maps.Geocoder();
   var address = document.getElementById('address').value;
   geocoder.geocode({'address': address}, function (results, status) {
 
-  if (status == google.maps.GeocoderStatus.OK) {
+    if (status == google.maps.GeocoderStatus.OK) {
 
-    crearmapa(results[0].geometry.location.lat().toFixed(6),results[0].geometry.location.lng().toFixed(6))
-    map.addMarker({ lat: results[0].geometry.location.lat().toFixed(6), lng: results[0].geometry.location.lng().toFixed(6)});   // marcador en [lat, lng]
-  //  window.open("http://api.openweathermap.org/data/2.5/weather?lat=" + results[0].geometry.location.lat().toFixed(6) + "&lon=" + results[0].geometry.location.lng().toFixed(6) + "&mode=html&appid=" + apikey + "&units=metric&lang=es","","status=no,directories=no,menubar=no,toolbar=no,scrollbars=no,location=no,resizable=no,titlebar=no, width=160, height=170, top=" + window.innerHeight/2 + ", left=" + window.innerWidth/2);       
-  
-  }else{ alert("Dirección no encontrada!!!"); }
+      crearmapa(results[0].geometry.location.lat().toFixed(6),results[0].geometry.location.lng().toFixed(6))
+      map.addMarker({ lat: results[0].geometry.location.lat().toFixed(6), lng: results[0].geometry.location.lng().toFixed(6)});   // marcador en [lat, lng]
+      
+    }else{ alert("Dirección no encontrada!!!"); }
 
   });
 }
@@ -101,7 +102,7 @@ function buscar() {   // buscamos las coordenadas de una direccion y la buscamos
 function buscador(e,valor){    // Busca la ciudad introducida cuando se pulsa enter
 
   tecla = (document.all) ? e.keyCode : e.which;
-  if (tecla==13) buscar();
+  if (tecla==13) buscar();   
 }
    
 window.addEventListener("load",comenzar,false);
